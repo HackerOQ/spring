4701f741c0835084158dbc442762645f4a4b5cb0
runoob <test@runoob.com>
2020-10-06 10:48:09 +0800

    Revert "jij"
    This reverts commit a7eddb1e

--- 02.Spring-Boot-Config/src/main/java/com/springboot/Application.java
+++ 02.Spring-Boot-Config/src/main/java/com/springboot/Application.java
@@ -1,45 +1,20 @@
 package com.springboot;
 
-
-
-
-
-import lombok.extern.log4j.Log4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import com.springboot.bean.ConfigBean;
 import com.springboot.bean.TestConfigBean;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-@Log4j
 @SpringBootApplication
 @EnableConfigurationProperties({ConfigBean.class,TestConfigBean.class})
 //@ImportResource({"classpath:some-application.xml"})
-public class Application extends SpringBootServletInitializer {
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
-	public static void main(String[] args) throws UnknownHostException {
-		ConfigurableApplicationContext application = SpringApplication.run(Application.class, args);
-		Environment env = application.getEnvironment();
-		String ip = InetAddress.getLocalHost().getHostAddress();
-		String port = env.getProperty("server.port");
-		String path = env.getProperty("server.servlet-path").trim();
-		log.info("\n----------------------------------------------------------\n\t" +
-				"Application Jeecg-Boot is running! Access URLs:\n\t" +
-				"Local: \t\thttp://localhost:" + port + path + "/\n\t" +
-				"External: \thttp://" + ip + ":" + port + path + "/\n\t" +
-				"Swagger-ui: \thttp://" + ip + ":" + port + path + "/swagger-ui.html\n\t" +
-				"Doc文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
-				"----------------------------------------------------------");
-
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setAddCommandLineProperties(false);
+		app.run(args);
 	}
 }
